cb7501159d39c5396425c2b2076f6d9472d94144
Alexk-Dev <118230157+Alexka-Dev@users.noreply.github.com>
2024-07-22 17:44:48 -0300

    Update nft.sol
    - 4 series of NFT's were added.
    - Each series has a maximum supply of 50 units.
    - Each unit will have a unique identifier.

--- contracts/nft.sol
+++ contracts/nft.sol
@@ -6,19 +6,37 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721URIStorage {
-   using Counters for Counters.Counter;
-   Counters.Counter private _tokenIds; 
-   constructor() ERC721("CollectionName", "DEV") {
-   }
+    using Counters for Counters.Counter;
 
-   function createToken(string memory tokenURI) public payable returns (uint) {
-      require(msg.value >= 100000000000000000, 'The minimum amount is 0.5 matic');
-      _tokenIds.increment;
-      uint256 newItemId = _tokenIds.current();
+    struct Series {
+        uint256 currentSupply;
+        uint256 maxSupply;
+    }
 
-      _mint(msg.sender, newItemId);
-      _setTokenURI(newItemId, tokenURI);
+    mapping(uint256 => Series) public series;
+    uint256 public constant NUM_SERIES = 4;
+    uint256 public constant SERIES_SUPPLY = 50;
 
-      return newItemId;
-   }
- }
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("CollectionName", "DEV") {
+        for (uint256 i = 1; i <= NUM_SERIES; i++) {
+            series[i] = Series(0, SERIES_SUPPLY);
+        }
+    }
+
+    function createToken(uint256 seriesId, string memory tokenURI) public payable returns (uint256) {
+        require(seriesId > 0 && seriesId <= NUM_SERIES, "Invalid series ID");
+        require(msg.value >= 5000000000000000000, "The minimum amount is 0.5 MATIC");
+        require(series[seriesId].currentSupply < SERIES_SUPPLY, "All tokens in this series have been minted");
+
+        series[seriesId].currentSupply++;
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
